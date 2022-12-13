e60ecae4b26562d1a464ac1cee33afd2afbafa55
DarioDC95 <dario_95@live.com>
2022-12-13 18:05:24 +0100

    created bonus Folder

---
+++ BONUS/js/script.js
@@ -0,0 +1,53 @@
+// MILESTONE 0
+// Creo un array di oggetti con le informazioni del soggetto
+const profiles = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        image: "wayne-barnet-founder-ceo.jpg"
+    },
+    
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        image: "angela-caroll-chief-editor.jpg"
+    },
+
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        image: "walter-gordon-office-manager.jpg"
+    },
+
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        image: "angela-lopez-social-media-manager.jpg"
+    },
+
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        image: "scott-estrada-developer.jpg"
+    },
+
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        image: "barbara-ramos-graphic-designer.jpg"
+    }
+]
+
+let profileDiv = document.getElementsByClassName('profile')
+console.log(profileDiv)
+
+// MILESTONE 1/2
+// Prendere gli elementi degli oggetti da dentro l'array e stamparli in console
+for (let i = 0; i < profiles.length; i++) {
+    console.log(profiles[i]);
+    for (const key in profiles[i]) {
+        // console.log(key + ":" + " " + profiles[i][key])
+        console.log(`${key}: ${profiles[i][key]}`)
+        profileDiv[i].innerHTML += `${key}: ${profiles[i][key]} <br>`
+    }
+}
---
+++ BONUS/readme.txt
@@ -0,0 +1,7 @@
+PROBLEMA: 
+
+Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+1- Creare un array di oggetti con le info da stampare nel DOM
+2- Estrarre gli oggetti dall'array 
+3- Inserire gli oggetti nei "div" del DOM
